abaa8dd98aa1c8d614a6b391e1aa82d15cbbc7ec
reskyner <reskyner@googlemail.com>
2018-11-01 13:32:10 +0000

    add config and parser

--- squonk_requests.py
+++ squonk_requests.py
@@ -1,75 +1,97 @@
 import requests
+import configparser
 
 
-def check_response(response):
-    if not response.status_code == 200:
-        response.raise_for_status()
+class SquonkRequests:
 
+    def __init__(self):
+        settings_file = 'config.ini'
+        settings = configparser.ConfigParser()
+        settings._interpolation = configparser.ExtendedInterpolation()
+        settings.read(settings_file)
 
-# uses https protocol
-def get_token(content_type, grant_type, client_id, client_secret, url):
+        # general settings
+        self.base_url = settings.get('general', 'base_url')
 
-    # -H option in curl request
-    headers = {
-        'Content-Type': content_type,
-    }
+        # config read for get_token
+        self.token_content_type = settings.get('token', 'content_type')
+        self.token_grant_type = settings.get('token', 'grant_type')
+        self.token_client_id = settings.get('token', 'client_id')
+        self.token_client_secret = settings.get('token', 'client_secret')
+        self.token_url = settings.get('token', 'url')
 
-    # data to pass: -d in curl request
-    data = {
-        'grant_type': grant_type,
-        'client_id': client_id,
-        'client_secret': client_secret,
-    }
+        # config read for list_service_ids
+        self.ids_endpoint = settings.get('ids', 'endpoint')
 
-    # post the request to the token url
-    response = requests.post(url, headers=headers, data=data, verify=False)
+    def check_response(self, response):
+        if not response.status_code == 200:
+            response.raise_for_status()
 
-    check_response(response)
+    # uses https protocol
+    def get_token(self):
 
-    # return token as string
-    token = str(response.json()['access_token'].decode('ascii'))
+        # -H option in curl request
+        headers = {
+            'Content-Type': self.token_content_type,
+        }
 
-    return token
+        # data to pass: -d in curl request
+        data = {
+            'grant_type': self.token_grant_type,
+            'client_id': self.token_client_id,
+            'client_secret': self.token_client_secret,
+        }
 
+        # post the request to the token url
+        response = requests.post(self.token_url, headers=headers, data=data, verify=False)
 
-# uses http protocol
-def list_service_ids(token, url):
+        self.check_response(response)
 
-    headers = {
-        'authorization': str('bearer ' + token)
-    }
+        # return token as string
+        token = str(response.json()['access_token'].decode('ascii'))
 
-    # post the request
-    response = requests.post(url, headers=headers, verify=False)
+        return token
 
-    check_response(response)
+    # uses http protocol
+    def list_service_ids(self, token):
 
-    out = [method['id'] for method in response.json()]
-    return out
+        url = str(self.base_url + '/' + self.ids_endpoint)
 
+        headers = {
+            'authorization': str('bearer ' + token),
+        }
 
-def post_job(token, base_url, job_service_endpoint, username, options, input_data, input_metadata, service_id):
+        # post the request
+        response = requests.post(url, headers=headers, verify=False)
 
-    url = str(base_url + '/' + job_service_endpoint + '/' + service_id)
+        self.check_response(response)
 
-    headers = {
-        'Authorization': str('bearer ' + token),
-        'Content-Type': 'multipart/mixed',
-        'SquonkUsername': username,
-    }
+        out = [method['id'] for method in response.json()]
 
-    files = {
-        'options': options,
-        'input_data': input_data,
-        'input_metadata': input_metadata,
-    }
+        return out
 
-    response = requests.post(url, headers=headers, files=files, verify=False)
+    def post_job(self, token, job_service_endpoint, username, options, input_data, input_metadata, service_id):
 
-    check_response(response)
+        url = str(self.base_url + '/' + job_service_endpoint + '/' + service_id)
 
-    job_id = str(response.json()['job_id'])
+        headers = {
+            'Authorization': str('bearer ' + token),
+            'Content-Type': 'multipart/mixed',
+            'SquonkUsername': username,
+        }
 
-    return job_id
+        files = {
+            'options': options,
+            'input_data': input_data,
+            'input_metadata': input_metadata,
+        }
+
+        response = requests.post(url, headers=headers, files=files, verify=False)
+
+        self.check_response(response)
+
+        job_id = str(response.json()['job_id'])
+
+        return job_id
 
 
