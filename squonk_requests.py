00bd3cb30243fc0bd1d74968a7dae05deabd5901
reskyner <reskyner@googlemail.com>
2018-11-01 14:31:30 +0000

    refactor to authorize automatically

--- squonk_requests.py
+++ squonk_requests.py
@@ -2,7 +2,12 @@ import requests
 import configparser
 
 
-class SquonkRequests:
+def check_response(response):
+    if not response.status_code == 200:
+        response.raise_for_status()
+
+
+class SquonkAuth:
 
     def __init__(self):
         settings_file = 'config.ini'
@@ -10,22 +15,12 @@ class SquonkRequests:
         settings._interpolation = configparser.ExtendedInterpolation()
         settings.read(settings_file)
 
-        # general settings
-        self.base_url = settings.get('general', 'base_url')
-
         # config read for get_token
         self.token_content_type = settings.get('token', 'content_type')
         self.token_grant_type = settings.get('token', 'grant_type')
         self.token_client_id = settings.get('token', 'client_id')
         self.token_client_secret = settings.get('token', 'client_secret')
         self.token_url = settings.get('token', 'url')
-
-        # config read for list_service_ids
-        self.ids_endpoint = settings.get('ids', 'endpoint')
-
-    def check_response(self, response):
-        if not response.status_code == 200:
-            response.raise_for_status()
 
     # uses https protocol
     def get_token(self):
@@ -45,15 +40,31 @@ class SquonkRequests:
         # post the request to the token url
         response = requests.post(self.token_url, headers=headers, data=data, verify=False)
 
-        self.check_response(response)
+        check_response(response)
 
         # return token as string
         token = str(response.json()['access_token'].decode('ascii'))
 
         return token
 
+
+class SquonkServiceInfo:
+
+    def __init__(self):
+        settings_file = 'config.ini'
+        settings = configparser.ConfigParser()
+        settings._interpolation = configparser.ExtendedInterpolation()
+        settings.read(settings_file)
+
+        # general settings
+        self.base_url = settings.get('general', 'base_url')
+
+        # config read for list_service_ids
+        self.ids_endpoint = settings.get('ids', 'endpoint')
+
+
     # uses http protocol
-    def list_service_ids(self, token):
+    def list_service_ids(self, token=SquonkAuth().get_token()):
 
         url = str(self.base_url + '/' + self.ids_endpoint)
 
@@ -64,19 +75,35 @@ class SquonkRequests:
         # post the request
         response = requests.post(url, headers=headers, verify=False)
 
-        self.check_response(response)
+        check_response(response)
 
         out = [method['id'] for method in response.json()]
 
         return out
 
-    def post_job(self, token, job_service_endpoint, username, options, input_data, input_metadata, service_id):
 
-        url = str(self.base_url + '/' + job_service_endpoint + '/' + service_id)
+class SubmitJob:
+
+    def __init__(self):
+        settings_file = 'config.ini'
+        settings = configparser.ConfigParser()
+        settings._interpolation = configparser.ExtendedInterpolation()
+        settings.read(settings_file)
+
+        # general settings
+        self.base_url = settings.get('general', 'base_url')
+
+        # config read for posting a job
+        self.job_post_endpoint = settings.get('job', 'endpoint')
+        self.job_post_content_type = settings.get('job', 'content_type')
+
+    def post_job(self, token, username, options, input_data, input_metadata, service_id):
+
+        url = str(self.base_url + '/' + self.job_post_endpoint + '/' + service_id)
 
         headers = {
             'Authorization': str('bearer ' + token),
-            'Content-Type': 'multipart/mixed',
+            'Content-Type': self.job_post_content_type,
             'SquonkUsername': username,
         }
 
@@ -88,7 +115,7 @@ class SquonkRequests:
 
         response = requests.post(url, headers=headers, files=files, verify=False)
 
-        self.check_response(response)
+        check_response(response)
 
         job_id = str(response.json()['job_id'])
 
