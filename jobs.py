4e96d1200cbdd3d65f032bbfe932132c429842b8
Skyner <reskyner@googlemail.com>
2018-11-19 13:38:29 +0000

    mols to dicts

--- jobs.py
+++ jobs.py
@@ -81,28 +81,3 @@ class SquonkJob:
         print(curlify.to_curl(response.request))
         print(response.content)
 
-
-def dict_from_mol(mol_string):
-    json_dict = {'uuid': str(uuid.uuid1()),
-                 'source': mol_string,
-                 'type': 'mol'}
-
-    return json_dict
-
-
-def sdf_to_mol_dicts(sdf_file):
-    suppl = Chem.SDMolSupplier(sdf_file)
-    mol_json_list = []
-    for mol in suppl:
-        m = dict_from_mol(Chem.MolToMolBlock(mol))
-        mol_json_list.append(m)
-
-    return mol_json_list
-
-
-def mol_to_mol_dict(mol_file):
-    with open(mol_file, 'r') as f:
-        mol_string = f.read()
-    mol_json = dict_from_mol(mol_string)
-
-    return mol_json
